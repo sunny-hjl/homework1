b7856cd116cc642be8c4f9eb3a58d993aaf156a9
sunny-hjl <1542427952@qq.com>
2020-04-27 10:11:19 +0800

    项目05_03

---
+++ src/main/java/com/mysql/jdbc/Driver.java
@@ -0,0 +1,38 @@
+package com.mysql.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class Driver implements java.sql.Driver {
+    public Connection connect(String s, Properties properties) throws SQLException {
+        return null;
+    }
+
+    public boolean acceptsURL(String s) throws SQLException {
+        return false;
+    }
+
+    public DriverPropertyInfo[] getPropertyInfo(String s, Properties properties) throws SQLException {
+        return new DriverPropertyInfo[0];
+    }
+
+    public int getMajorVersion() {
+        return 0;
+    }
+
+    public int getMinorVersion() {
+        return 0;
+    }
+
+    public boolean jdbcCompliant() {
+        return false;
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+}
